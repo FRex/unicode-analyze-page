551e28a03f9973080c6afdf7dc1863409379e55a
FRex <rex2k8@gmail.com>
2020-07-11 19:44:44 +0200

    printing char totals when generating data

--- gendata.py
+++ gendata.py
@@ -15,9 +15,14 @@ for line in code:
     idx = 10 if parts[1] == '<control>' else 1
     out.append((int(parts[0], 16), parts[idx]))
 
+chars = len(out)
 for line in uhan:
     parts = line.split(maxsplit=2)
     if len(parts) == 3 and parts[1] == 'kDefinition':
         out.append((int(parts[0][2:], 16), parts[2]))
 
+unihan = len(out) - chars
+print("chars: ", chars)
+print("unihan chars: ", unihan)
+print("total: ", chars + unihan)
 open('data.js', 'w').write("data=JSON.parse('" + json.dumps(out).replace("'", "\\'") + "')\n")
